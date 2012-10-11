8667ba51a2a993b01da5061b06b5c977caf0d2fa
Emiliano Silva Barbosa <emilianoeloi@gmail.com>
2012-10-11 00:27:46 -0300

    Documentos / Bean: MedicoBean, PacienteBean
    Coloquei alguns documentos do peojeto.
    Criei as Beans Medico e Paciente

---
+++ src/java/medico/MedicoBean.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package medico;
+
+import pessoa.*;
+
+/**
+ *
+ * @author emilianoeloi
+ */
+public class MedicoBean extends PessoaBean {
+
+    public MedicoBean() {
+        super();
+    }
+    
+    private String Crm;
+
+    public String getCrm() {
+        return Crm;
+    }
+
+    public void setCrm(String Crm) {
+        this.Crm = Crm;
+    }
+    
+}
---
+++ src/java/paciente/PacienteBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paciente;
+
+import pessoa.*;
+
+/**
+ *
+ * @author emilianoeloi
+ */
+public class PacienteBean extends PessoaBean {
+
+    public PacienteBean() {
+        super();
+    }
+    
+}
