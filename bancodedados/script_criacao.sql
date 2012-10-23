0dab28a9090beaaa99e570c5fbd9bf4986ac3401
Emiliano Silva Barbosa <emilianoeloi@gmail.com>
2012-10-23 01:12:26 -0200

    Banco de dados
    Campo de email UNIQUE.
    CAmpo de Data de cadastro com preenchimento automático

--- bancodedados/script_criacao.sql
+++ bancodedados/script_criacao.sql
@@ -3,12 +3,18 @@ CREATE TABLE hospitais{
 	Codigo_Hospital SERIAL PRIMARY KEY,
 	Nome_Hospital VARCHAR(25)
 )
+DROP TABLE pessoas;
+-- DROP TABLE pessoas
 CREATE TABLE pessoas(
 	Codigo_Pessoa SERIAL PRIMARY KEY,
 	Nome_Pessoa VARCHAR(100),
 	Cpf_Pessoa CHAR(11) UNIQUE,
-	Email_Pessoa VARCHAR(200),
+	Email_Pessoa VARCHAR(200) UNIQUE,
 	Id_Pessoa VARCHAR(10),
 	Data_Nascimento_Pessoa TIMESTAMP,
-	Senha_Pessoa VARCHAR(255)
+	Senha_Pessoa VARCHAR(255),
+	Data_Cadastro TIMESTAMP DEFAULT current_timestamp
 )
+
+select *
+  from PESSOAS
