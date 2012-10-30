5df11c17dd35c2dc1996659fa4878acee445d6e1
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2012-10-30 01:21:04 -0200

    EXAME
    ExameForm.jsp

--- src/java/exame/ExameDAO.java
+++ src/java/exame/ExameDAO.java
@@ -32,13 +32,47 @@ public class ExameDAO {
         return con;
     }
     
-    public void cadastrar(ExameBean exameBean){
-        try{
-            String query = "INSERT INTO exames (Nome_Exame, Descricao_exame) VALUES (?, ?)";
+    private void obterProximoCodigo(ExameBean exame){
+                    
+        try{    
+            
+            String query = "SELECT NEXTVAL ('exames_codigo_exame_seq') AS codigo";
             this.con = getConexao();
             this.ptmt = con.prepareStatement(query);
-            this.ptmt.setString(1, exameBean.getNome());
-            this.ptmt.setString (2, exameBean.getDescricao());
+            this.rs = this.ptmt.executeQuery();
+            
+            while(rs.next()){
+                exame.setCodigo(rs.getInt(1));
+            }
+            
+        }catch(SQLException e){
+            e.printStackTrace();
+        }finally{
+            try{
+                if(this.rs!=null)
+                    rs.close();
+                if(this.ptmt!=null)
+                    ptmt.close();
+                if(this.con!=null)
+                    this.con.close();
+            }catch(SQLException e){
+                e.printStackTrace();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    
+    public void cadastrar(ExameBean exameBean){
+        try{
+            String query = "INSERT INTO exames (codigo_exame, Nome_Exame, Descricao_exame) VALUES (?,?, ?)";
+            this.obterProximoCodigo(exameBean);
+            this.con = getConexao();
+            this.ptmt = con.prepareStatement(query);
+            this.ptmt.setInt(1, exameBean.getCodigo());
+            this.ptmt.setString(2, exameBean.getNome());
+            this.ptmt.setString (3, exameBean.getDescricao());
             this.ptmt.executeUpdate();
         }catch(SQLException e){
             e.printStackTrace();
