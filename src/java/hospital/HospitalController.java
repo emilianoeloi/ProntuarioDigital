454f624f16bbfab3c0f6da123802e85f831fc905
Emiliano Silva Barbosa <emilianoeloi@gmail.com>
2012-11-04 14:44:19 -0200

    Revert "Revert "TESTE""
    This reverts commit 9d05f6acbc79e166f934838437710bc3c69ba794.

--- src/java/hospital/HospitalController.java
+++ src/java/hospital/HospitalController.java
@@ -13,72 +13,92 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.util.Enumeration;
-import java.util.List;
-import javax.servlet.RequestDispatcher;
 
 /**
  *
  * @author emilianoeloi
  */
 @WebServlet(name = "HospitalController", urlPatterns = {"/HospitalController"})
-public class HospitalController extends HttpServlet{ 
+public class HospitalController extends HttpServlet {
 
-    protected void service (HttpServletRequest request, HttpServletResponse response)
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     * 
+     * 
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-        HospitalDAO dao = new HospitalDAO();
-        HospitalBean hospital = new HospitalBean();
-         
-        String acao = request.getParameter("acao");
-        
-        if (acao == null){
-            acao = "listar";
-        }
-        
-                
-        if(acao != null){
-           if (request.getParameter("codigo") != null && request.getParameter("codigo") !="" ){
-               hospital.setCodigo(Integer.parseInt(request.getParameter("codigo"))); 
-           }
-           
-           hospital.setNome(request.getParameter("nome"));
-
-        }
-        
-       try {
-           RequestDispatcher rd = null;
-           if (acao.equalsIgnoreCase("cadastrar")){
-               dao.cadastrar(hospital);
-               request.setAttribute("hospitalSelecionado", hospital);
-               rd = request.getRequestDispatcher("hospitalForm.jsp");
-               
-           } else if(acao.equalsIgnoreCase("listar")) {
-               List hospitalList = dao.retornarTodos();
-               request.setAttribute("hospitalList", hospitalList);
-               rd = request.getRequestDispatcher("hospitalLista.jsp");
-                              
-           } else if (acao.equalsIgnoreCase("excluir")){
-               dao.excluir(hospital);
-               rd = request.getRequestDispatcher("HospitalController?acao=listar");
-           
-           } else if (acao.equalsIgnoreCase("obterum")){
-            
-               hospital = dao.retornarPeloCodigo(hospital.getCodigo());
-               request.setAttribute("hospitalSelecionado", hospital);
-               rd = request.getRequestDispatcher("HospitalController?acao=formulario");
-
-           } else if(acao.equalsIgnoreCase("Formulario")) {
-               rd = request.getRequestDispatcher("hospitalForm.jsp");
-           
-           } else if (acao.equalsIgnoreCase("alterar")){
-               dao.alterar(hospital);
-               request.setAttribute("hospitalSelecionado", hospital);
-               rd = request.getRequestDispatcher("HospitalController?acao=formulario");
-           }
-           rd.forward(request, response);
-        } catch (Exception e) {            
-            e.printStackTrace();
-            throw new ServletException(e);
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            Enumeration e = request.getAttributeNames();
+            String dados = "";
+            while(e.hasMoreElements()){
+                String parametro = (String)e.nextElement();
+                dados += "<li>" + parametro + ": " + request.getParameter(parametro) + "</li>";
+            }
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet HospitalController</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Ta OSSO: " + request.getContextPath() + "</h1>");
+            out.println("<ul>");
+            out.println(dados);
+            out.println("</ul>");
+            out.println("</body>");
+            out.println("</html>");
+        } finally {            
+            out.close();
         }
     }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
 }
